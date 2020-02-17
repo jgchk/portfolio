bc095dbde0be002c3abe928c3a712ed224f1dfb6
Jake Cheek <jake@f-m.fm>
2020-02-16 23:17:51 -0500

    re-add airbnb preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   extends: [
+    'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
     'plugin:@typescript-eslint/eslint-recommended',
