14bbd9a499717d58212c4def02d946cf6715d56b
Jake Cheek <jake@f-m.fm>
2020-03-13 01:00:59 -0400

    style album tab and buttons

--- components/Music/AlbumButton/index.tsx
+++ components/Music/AlbumButton/index.tsx
@@ -1,13 +1,19 @@
 import React, { FunctionComponent } from 'react'
 
 import { Album } from '../../../lib/api/aws'
+import styles from './styles.less'
 
 type AlbumButtonProps = {
   album: Album
 }
 
 const AlbumButton: FunctionComponent<AlbumButtonProps> = ({ album }) => (
-  <button type='button'>{album.title}</button>
+  <button className={styles.button} type='button'>
+    {album.cover && (
+      <img className={styles.image} src={album.cover.url} alt='cover' />
+    )}
+    <div className={styles.title}>{album.title}</div>
+  </button>
 )
 
 export default AlbumButton
--- components/Music/AlbumsTab/index.tsx
+++ components/Music/AlbumsTab/index.tsx
@@ -2,13 +2,14 @@ import React, { FunctionComponent } from 'react'
 
 import AlbumButton from '../AlbumButton'
 import { Album } from '../../../lib/api/aws'
+import styles from './styles.less'
 
 type AlbumsTabProps = {
   albums: Album[]
 }
 
 const AlbumsTab: FunctionComponent<AlbumsTabProps> = ({ albums }) => (
-  <div>
+  <div className={styles.container}>
     {albums.map(album => (
       <AlbumButton key={album.id} album={album} />
     ))}
