8b16948b88cea867ab69417ae5c535cc29cb00f4
Jake Cheek <jake@f-m.fm>
2020-03-12 23:50:08 -0400

    style artist tab and buttons

--- components/Music/ArtistButton/index.tsx
+++ components/Music/ArtistButton/index.tsx
@@ -1,13 +1,16 @@
 import React, { FunctionComponent } from 'react'
 
 import { Artist } from '../../../lib/api/aws'
+import styles from './styles.less'
 
 type ArtistButtonProps = {
   artist: Artist
 }
 
 const ArtistButton: FunctionComponent<ArtistButtonProps> = ({ artist }) => (
-  <button type='button'>{artist.name}</button>
+  <button className={styles.button} type='button'>
+    {artist.name}
+  </button>
 )
 
 export default ArtistButton
--- components/Music/ArtistsTab/index.tsx
+++ components/Music/ArtistsTab/index.tsx
@@ -2,13 +2,14 @@ import React, { FunctionComponent } from 'react'
 
 import ArtistButton from '../ArtistButton'
 import { Artist } from '../../../lib/api/aws'
+import styles from './styles.less'
 
 type ArtistsTabProps = {
   artists: Artist[]
 }
 
 const ArtistsTab: FunctionComponent<ArtistsTabProps> = ({ artists }) => (
-  <div>
+  <div className={styles.container}>
     {artists.map(artist => (
       <ArtistButton key={artist.id} artist={artist} />
     ))}
