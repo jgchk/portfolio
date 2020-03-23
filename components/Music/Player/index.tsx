30f47d8d9800cfd1f86e424c5ca2cd55aec6d2a9
Jake Cheek <jake@f-m.fm>
2020-03-23 18:51:52 -0400

    style fonts and album art

--- components/Music/Player/index.tsx
+++ components/Music/Player/index.tsx
@@ -3,7 +3,15 @@ import React, { FunctionComponent } from 'react'
 import styles from './styles.less'
 
 const Player: FunctionComponent<{}> = () => {
-  return <div className={styles.container} />
+  return (
+    <div className={styles.container}>
+      <div className={styles.cover} />
+      <div className={styles.trackInfo}>
+        <div className={styles.trackName}>Track Name</div>
+        <div className={styles.trackArtist}>Artist</div>
+      </div>
+    </div>
+  )
 }
 
 export default Player
